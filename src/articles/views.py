81750a9777b09f3fbdbfa56258dd7bd587910e26
AlexanderPAI <lawlikeart@yandex.ru>
2024-02-24 03:33:48 +0300

    Templates: tags have been fixed

--- src/articles/views.py
+++ src/articles/views.py
@@ -15,10 +15,12 @@ def paginator(request, articles):
 
 def index(request):
     """Представление главной страницы."""
+    tags = Tag.objects.all()
     articles = Article.objects.all()
     page_obj = paginator(request, articles)
     context = {
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'tags': tags,
     }
     return render(
         request,
@@ -29,32 +31,38 @@ def index(request):
 
 def article(request, article_id):
     """Представление одной статьи."""
+    tags = Tag.objects.all()
     article = get_object_or_404(Article, pk=article_id)
     context = {
-        'article': article
+        'article': article,
+        'tags': tags,
     }
     return render(request, 'articles/article.html', context)
 
 
 def articles_by_tag(request, slug):
     """Представление фильтра по тегу."""
+    tags = Tag.objects.all()
     tag = get_object_or_404(Tag, slug=slug)
     articles = tag.articles.all()
     page_obj = paginator(request, articles)
     context = {
-        'tag': tag,
         'page_obj': page_obj,
+        'tag': tag,
+        'tags': tags,
     }
     return render(request, 'articles/articles_by_tag.html', context)
 
 
 def articles_by_category(request, slug):
     """Представление фильтра по разделу."""
+    tags = Tag.objects.all()
     category = get_object_or_404(Category, slug=slug)
     articles = category.articles.all()
     page_obj = paginator(request, articles)
     context = {
+        'page_obj': page_obj,
         'category': category,
-        'page_obj': page_obj
+        'tags': tags,
     }
     return render(request, 'articles/articles_by_category.html', context)
